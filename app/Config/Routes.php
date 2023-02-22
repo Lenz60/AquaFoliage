560f992524ec7b2d1549e9dc5db7c2aec785c570
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2023-02-23 06:29:23 +0700

    Added more routes and remove setDefaultMethod(index)

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,14 +12,14 @@ $routes = Services::routes();
  */
 $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
+$routes->setDefaultMethod('');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
 // Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
-// $routes->setAutoRoute(false);
+// $routes->setAutoRoute(true);
 
 /*
  * --------------------------------------------------------------------
@@ -32,8 +32,9 @@ $routes->set404Override();
 $routes->get('/', 'HomeController::index');
 // $routes->post('/login', 'LoginController::index');
 $routes->get('/login', 'LoginController::index');
-$routes->post('/saveLogin', 'LoginController::saveLogin');
-
+$routes->post('/login', 'LoginController::login');
+$routes->get('/register', 'RegisterController::index');
+$routes->post('/register', 'RegisterController::register');
 
 /*
  * --------------------------------------------------------------------
