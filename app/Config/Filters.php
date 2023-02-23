27faa91d56532a8f1c12cc899cd9f75f9da431d9
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2023-02-24 04:35:53 +0700

    Filtering token for JWT Authorization

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,6 +2,7 @@
 
 namespace Config;
 
+use App\Filters\JwtFilter;
 use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
@@ -21,6 +22,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'auth'          => JwtFilter::class
     ];
 
     /**
@@ -34,7 +36,7 @@ class Filters extends BaseConfig
             // 'invalidchars',
         ],
         'after' => [
-            'toolbar',
+            // 'toolbar',
             // 'honeypot',
             // 'secureheaders',
         ],
@@ -60,5 +62,11 @@ class Filters extends BaseConfig
      * Example:
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
-    public array $filters = [];
+    public array $filters = [
+        'auth' => [
+            'before' => [
+                '/'
+            ]
+        ]
+    ];
 }
---
+++ app/Filters/JwtFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class JwtFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+
+        if (!isset($_COOKIE['COOKIE-SESSION'])) {
+            return redirect()->to('/login');
+        } else {
+            //! Header check removed due to saving to cookie session
+            // $header = $request->getServer('HTTP_AUTHORIZATION'); 
+            try {
+                helper('jwt');
+                // $encodedToken = getJWT($header);
+                //? Automatically detect header from Cookie
+                $encodedToken = $_COOKIE['COOKIE-SESSION'];
+                validateJWT($encodedToken);
+                return $request;
+            } catch (Exception $e) {
+                return Services::response()->setJson([
+                    'error' => $e->getMessage()
+                ])->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED);
+            }
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do something here
+    }
+}
