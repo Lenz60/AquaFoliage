9f6892853f65156cf31703c19c05314cdb7c5d78
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2023-03-16 02:39:35 +0700

    Checking the token, if the token is in the blacklisted list reset the cookie-session, if the token is not in the blacklisted list, then continue to view

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,11 +2,19 @@
 
 namespace App\Controllers;
 
+use App\Models\BlacklistModel;
+
 
 class HomeController extends BaseController
 {
     public function index()
     {
-        return view('pages/home');
+        $blacklistModel = new BlacklistModel();
+        $currentCookie = $_COOKIE['COOKIE-SESSION'];
+        // dd($currentCookie);
+        $checkBlacklist = $blacklistModel->checkJwtBlacklist($currentCookie);
+        if (!$checkBlacklist) {
+            return view('pages/home');
+        }
     }
 }
