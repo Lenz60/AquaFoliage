59cad14a2f41fb7873e93d777dd5bc27023852de
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2023-03-16 02:37:07 +0700

    checking if the its logout because of token expired, then display a alert

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -13,13 +13,11 @@
     <title>Aqua Foliage</title>
     <script src="/node_modules/flowbite/dist/flowbite.min.js"></script>
     <!-- // * BeginNavbar -->
-
-
     <nav class="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
             <a href="#" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Aqua Folliage</span>
             </a>
             <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
@@ -74,8 +72,34 @@
 </head>
 
 <body>
+    <?php if (!isset($_COOKIE['COOKIE-EXPIRED'])) {
+    ?>
+        <div class="flex p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800" role="alert">
+            <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>
+                <span class="font-medium">Session Expired,</span> Please login again.
+            </div>
+        </div>
+    <?php
+    } elseif (isset($_COOKIE['COOKIE-BLACKLISTED'])) {
+    ?>
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <span class="block sm:inline">Invalid login, Please login properly.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <title>Close</title>
+                    <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+            </span>
+        </div>
+    <?php
+    }
+    ?>
+
     <?= $this->renderSection('content'); ?>
-    <script src="/node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
 <footer class=" bg-white rounded-lg shadow m-4 dark:bg-gray-800">
     <div class="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
