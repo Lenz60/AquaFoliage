57b4ca2ed053b55cc4e39bc73f9632df264c82df
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2023-03-17 04:58:44 +0700

    Temporary using about page for designing the homepage UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        home1 : "url('https://i.imgur.com/owjKKIa.png')",
+        home2 : "url('https://i.imgur.com/Q4Jq9Ce.png')",
+      },
+      fontFamily:{
+        kreon: "Kreon",
+        poppins: "Poppins"
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
