5faafbba80266d2b2914d7f2521b95fecd854bd4
Lenz60 <raflyandrian@gmail.com>
2023-03-30 23:18:47 +0700

    First Commit Laravel ver

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+</head>
+<body class="font-serif text-blue-500 font-bold">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod neque, necessitatibus maxime consequatur rem ullam aliquam! Minima dignissimos rem, delectus tempora suscipit magni totam? Placeat officiis nobis quas asperiores earum!</p>
+</body>
+</html>
