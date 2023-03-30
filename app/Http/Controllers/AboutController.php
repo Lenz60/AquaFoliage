2f534fd6ee31eb70c848162851c03f53877e5e63
Lenz60 <raflyandrian@gmail.com>
2023-03-31 03:04:18 +0700

    how to pass var from model and controller to view

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\AboutModel;
+
+class AboutController extends Controller
+{
+    //
+    public function info()
+    {
+        $model = new AboutModel();
+        // $a = $model->Info();
+        // $data['a'] = $model->Info();
+        $data['a'] = AboutModel::Info();
+        $data['b'] = "This data is from controller";
+
+        return view('about', $data);
+    }
+}
