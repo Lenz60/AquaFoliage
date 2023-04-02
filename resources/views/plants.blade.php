f195fbf26e8d3e4d306885e2bddd51e0ae9d5340
Lenz60 <raflyandrian@gmail.com>
2023-04-03 00:41:15 +0700

    loop the data to show all the  plants

---
+++ resources/views/plants.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.plants')
+@section('plantsContainer')
+<div>
+    @foreach ($plants as $plant)
+    <h1 class="text-3xl">{{ $plant->name }}</h1>
+    <br>
+    <div>
+        <h2 class="text-m font-bold">Scientific </h2>
+        <p>Genus : {{ $plant->genus }}</p>
+        <p>Species : {{ $plant->species }}</p>
+        <p>Common Name : {{ $plant->common_name }}</p>
+    </div>
+    <div>
+        <h2 class="text-m font-bold">Scientific </h2>
+        <p>Light : {{ $plant->light }}</p>
+        <p>Temp : {{ $plant->temp }}</p>
+        <p>Difficulty : {{ $plant->difficulty }}</p>
+        <p>Usage : {{ $plant->usage }}</p>
+    </div>
+    <br>
+    <p class="font-serif">{{ $plant->body }}</p>
+    <br>
+    @endforeach
+</div>
+@endsection
