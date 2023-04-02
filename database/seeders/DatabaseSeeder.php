a6bacb45b9b74c0e99f43398592d94e2a0363c46
Lenz60 <raflyandrian@gmail.com>
2023-04-03 00:40:35 +0700

    Seed the database with faker data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Plants;
+use App\Models\User;
+use Database\Factories\PlantFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(5)->create();
+        Plants::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
