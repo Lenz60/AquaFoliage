ad91f541261f7a0040d3589b8d5e40801d3c5437
Rafly Andrian <raflyandrian@gmail.com>
2023-06-24 02:25:55 +0700

    Designing Home

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+        <Hero> </Hero>
+        <!-- <SinglePages :fish="fish"></SinglePages> -->
+    </div>
+</template>
+
+<script>
+import Hero from "./Components/Hero.vue";
+import Navbar from "./Components/Navbar.vue";
+// import SinglePages from "./Components/SinglePages.vue";
+export default {
+    components: {
+        Navbar,
+        Hero,
+    },
+    data() {
+        return {
+            fish: 0,
+        };
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
