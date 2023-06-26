4dc8452c16b195339e4a9da9fdb1e9cc0fb7e8ef
Rafly Andrian <raflyandrian@gmail.com>
2023-06-27 04:28:33 +0700

    Animate poem is done

--- resources/js/Pages/components/Poem.vue
+++ resources/js/Pages/components/Poem.vue
@@ -1,32 +1,46 @@
 <template>
     <div data-themes="foliage">
         <div
-            class="hero min-h-screen"
+            id="poemWrap"
+            class="hero min-h-screen bg-opacity-60 absolute bg-base-200"
             style="background-image: url(/images/cinematic2.png)"
         >
-            <div class="bg-opacity-60 bg-base-200 px-[20px] py-[170px]">
+            <div class="hero-overlay bg-opacity-40 bg-base-200"></div>
+            <div class="flex-row">
                 <div class="hero-content items-center text-center">
                     <div class="indicator">
                         <span
-                            class="indicator-item font-catamaran indicator-start text-[96px]"
+                            id="quote1"
+                            class="indicator-item text-white font-catamaran indicator-start text-[96px]"
                             >“</span
                         >
-                        <p
-                            class="font-montserrat text-primary font-medium text-[32px]"
-                        >
-                            The beauty of nature is not just in the plants and
-                            animals themselves, but also in the way they
-                            interact with each other and with their environment.
-                        </p>
+                        <div>
+                            <p
+                                id="poem1"
+                                class="font-montserrat indent-8 text-primary font-medium text-[32px] overflow-hidden whitespace-nowrap"
+                            >
+                                The beauty of nature is not just in the plants
+                                and animals themselves,
+                            </p>
+                            <p
+                                id="poem2"
+                                class="font-montserrat text-primary font-medium text-[32px] overflow-hidden whitespace-nowrap"
+                            >
+                                but also in the way they interact with each
+                                other and with their environment.
+                            </p>
+                        </div>
                         <span
-                            class="indicator-item font-catamaran indicator-bottom text-[96px]"
+                            id="quote2"
+                            class="indicator-item pt-[60px] text-white font-catamaran indicator-bottom text-[96px]"
                             >”</span
                         >
                     </div>
                 </div>
                 <div class="text-end">
                     <h1
-                        class="m-5 ml-5 font-montserrat font-semibold text-[32px] text-primary"
+                        id="takashi"
+                        class="m-5 ml-5 font-montserrat font-semibold text-[32px] text-white"
                     >
                         - Takashi Amano
                     </h1>
@@ -37,8 +51,51 @@
 </template>
 
 <script>
+import $ from "jquery";
+import { onMounted } from "vue";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 export default {
     setup() {
+        onMounted(() => {
+            gsap.registerPlugin(ScrollTrigger);
+
+            // gsap.timeline({})
+            gsap.from("#poem1", {
+                scrollTrigger: "#poemWrap",
+                width: 0,
+                ease: "none",
+                delay: 0.5,
+                stagger: 1,
+                duration: 3,
+            });
+            gsap.from("#poem2", {
+                scrollTrigger: "#poemWrap",
+                width: 0,
+                delay: 3.5,
+                ease: "none",
+                stagger: 1,
+                duration: 3,
+            });
+
+            gsap.from("#quote1", {
+                scrollTrigger: "#poemWrap",
+                delay: 6.5,
+                opacity: 0,
+            });
+            gsap.from("#quote2", {
+                scrollTrigger: "#poemWrap",
+                delay: 6.5,
+                opacity: 0,
+            });
+            gsap.from("#takashi", {
+                scrollTrigger: "#poemWrap",
+                delay: 7,
+                opacity: 0,
+                stagger: 1,
+                duration: 2,
+            });
+        });
         return {};
     },
 };
