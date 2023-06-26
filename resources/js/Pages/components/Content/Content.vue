e209a82e0b117a3fdd312400e88ca9efb7c26333
Rafly Andrian <raflyandrian@gmail.com>
2023-06-27 00:53:41 +0700

    Section2 design mockup is done

--- resources/js/Pages/components/Content/Content.vue
+++ resources/js/Pages/components/Content/Content.vue
@@ -1,43 +1,103 @@
 <template>
-    <div class="flex max-w-fit">
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-            <figure>
-                <img
-                    src="/images/section2/staghornAlgae.jpg"
-                    class="w-[500px]"
-                    alt="Album"
-                />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title text-6xl">🐟</h2>
-                <p>
-                    The significance of such problems is so obvious that new
-                    model of the organisational activity requires the analysis
-                    of economic practicability of decisions made.
-                </p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Listen</button>
+    <div
+        class="flex-row max-w-fit bg-[#101310] bg-no-repeat bg-left-bottom min-h-screen relative border-2 border-red-600"
+        style="background-image: url(/images/section2bg.png)"
+    >
+        <div
+            class="flex justify-center items-center border-2 border-green-400 m-5"
+        >
+            <div
+                class="flex justify-center items-center m-5 p-5 border-2 border-blue-500"
+            >
+                <div
+                    class="w-[50%] flex justify-center items-center border-2 p-5 m-5 border-yellow-400"
+                >
+                    <img
+                        src="https://preview.redd.it/the-sacabambaspis-an-extinct-genus-of-jawless-fish-v0-mn6hrm3yk5sa1.jpg?auto=webp&s=fff00db26e0b861d1a4beff64ae3398a11aa68a1"
+                        alt=""
+                        class="rounded-md drop-shadow-2xl card w-[300px] h-[300px]"
+                    />
+                </div>
+                <div
+                    class="w-[50%] flex justify-center items-center border-2 p-5 m-5 border-yellow-400"
+                >
+                    <p
+                        class="indent-8 relative font-montserrat drop-shadow-xl text-white"
+                    >
+                        On the other hand the innovational path that we have
+                        chosen allows to appreciate the value of the tasks given
+                        by the society and government. The significance of such
+                        problems is so obvious that new model of the
+                        organisational activity requires the analysis of the
+                        step-by-step consistent development of the society. It
+                        should be noted that the implementation of the planned
+                        tasks plays an important part in the formation of the
+                        step-by-step consistent development of the society.
+                    </p>
                 </div>
             </div>
         </div>
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-            <figure>
-                <img
-                    src="/images/section2/javaFernMelting.jpg"
-                    class="w-[500px]"
-                    alt="Album"
-                />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title text-6xl">🦐</h2>
-                <p>
-                    The significance of such problems is so obvious that the
-                    implementation of the planned tasks allows to complete
-                    important implementation tasks of strengthening the
-                    democratic system.
-                </p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Listen</button>
+        <div class="flex-row items-center border-2 border-green-400 m-5">
+            <div
+                class="flex justify-center items-center m-5 p-5 border-2 border-blue-500"
+            >
+                <div
+                    class="w-[50%] flex justify-center items-center rt border-2 p-5 m-5 border-yellow-400"
+                >
+                    <p
+                        class="indent-8 relative font-montserrat drop-shadow-xl text-white"
+                    >
+                        The significance of such problems is so obvious that the
+                        implementation of the planned tasks contributes to the
+                        preparing and implementation of further ways of
+                        development. There's no doubt that the innovational path
+                        that we have chosen requires the analysis of existing
+                        financial and administrative conditions. The
+                        significance of such problems is so obvious that the
+                        understanding of nature of resource saving technologies
+                        directly depends on strengthening the democratic system.
+                    </p>
+                </div>
+                <div
+                    class="w-[50%] flex justify-center items-center border-2 p-5 m-5 border-yellow-400"
+                >
+                    <img
+                        src="https://preview.redd.it/the-sacabambaspis-an-extinct-genus-of-jawless-fish-v0-mn6hrm3yk5sa1.jpg?auto=webp&s=fff00db26e0b861d1a4beff64ae3398a11aa68a1"
+                        alt=""
+                        class="rounded-md drop-shadow-2xl card w-[300px] h-[300px]"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="flex-row items-center border-2 border-green-400 m-5">
+            <div
+                class="flex justify-center items-center m-5 p-5 border-2 border-blue-500"
+            >
+                <div
+                    class="w-[50%] flex justify-center items-center border-2 p-5 m-5 border-yellow-400"
+                >
+                    <img
+                        src="https://preview.redd.it/the-sacabambaspis-an-extinct-genus-of-jawless-fish-v0-mn6hrm3yk5sa1.jpg?auto=webp&s=fff00db26e0b861d1a4beff64ae3398a11aa68a1"
+                        alt=""
+                        class="rounded-md drop-shadow-2xl card w-[300px] h-[300px]"
+                    />
+                </div>
+                <div
+                    class="w-[50%] flex justify-center items-center border-2 p-5 m-5 border-yellow-400"
+                >
+                    <p
+                        class="indent-8 relative font-montserrat drop-shadow-xl text-white"
+                    >
+                        However, one should not forget constant quantitive
+                        growth and the scope of our activity requires the
+                        clarification of the tasks given by the society and
+                        government. Thus the innovational path that we have
+                        chosen contributes to increasing the quality of new
+                        propositions. The significance of such problems is so
+                        obvious that the innovational path that we have chosen
+                        allows to complete important implementation tasks of the
+                        system of mass participation.
+                    </p>
                 </div>
             </div>
         </div>
