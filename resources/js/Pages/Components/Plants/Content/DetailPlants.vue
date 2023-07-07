11518ba87da308d030857cc7424ca2f1afc17518
Rafly Andrian <raflyandrian@gmail.com>
2023-07-08 00:31:32 +0700

    added Detail Plants components

---
+++ resources/js/Pages/Components/Plants/Content/DetailPlants.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Coming soon..</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
