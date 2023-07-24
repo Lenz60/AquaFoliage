6032ea07be91b15e4bfbdab1080129307faa6204
Rafly Andrian <raflyandrian@gmail.com>
2023-07-25 00:11:14 +0700

    Added CSRF

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Istok+Web&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Catamaran&display=swap" rel="stylesheet">
+    <meta name="csrf-token" content="ubRt4jwLGAuv1MKmVynMyqosbcayoyMC4Gd88Epo">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     @routes
