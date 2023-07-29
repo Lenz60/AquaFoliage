d74b3f4cf50abcd2bfe3b6929e2c52e7bd64b129
Rafly Andrian <raflyandrian@gmail.com>
2023-07-30 00:48:54 +0700

    added relationship for each categories

---
+++ database/migrations/2023_07_29_154918_fav_plants.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('favPlants', function (Blueprint $table) {
+            $table->string('id');
+            $table->foreign('plants_id')
+            ->references('id')->on('plants')->onDelete('cascade');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
