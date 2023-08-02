d2ea983a90dfdc5d998fb6cf078354a443e88509
Rafly Andrian <raflyandrian@gmail.com>
2023-08-02 23:21:14 +0700

    Mockup for dashboard menu layout

---
+++ resources/js/Pages/Components/Plants/Submenu/DashboardMenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul class="menu w-56 rounded-box">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+            <li><a>Item 3</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
