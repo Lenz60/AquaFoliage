474a713e61e71920c6d70ab745ca0f9ad543feae
Rafly Andrian <raflyandrian@gmail.com>
2023-08-02 23:18:21 +0700

    Section for Dashboard

---
+++ resources/js/Pages/Components/Dashboard/Section.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div data-themes="foliage">
+            <div
+                class="flex flex-col h-screen overflow-x-auto no-scrollbar p-2"
+            >
+                <div class="p-5 border-2 border-yellow-300">
+                    <p>asdads</p>
+                    <p>asdads</p>
+                    <p>asdads</p>
+                    <p>asdads</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
