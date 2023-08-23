9b70f1a89ba8c9eabfbf70c14a43e0f931b1cab3
Rafly Andrian <raflyandrian@gmail.com>
2023-08-24 04:07:53 +0700

    added seeder for all favorites

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,25 +20,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
         //? Seeder for Relationship Favourites table
         //TODO: Seed for another Table with Favourites table
 
-        // $users = User::factory()
-        //     ->count(5)
-        //     ->create();
-        // $plants = Plants::factory()
-        //     ->count(5)
-        //     ->create();
-        // //v Seed The Favourites table
-        // foreach ($users as $user){
-        //     foreach ($plants as $plant){
-        //         FavPlant::factory([
-        //             'user_id' => $user->id,
-        //             'plants_id' => $plant->id
-        //         ]) ->create();
-        //     }
-        // }
+        $users = User::factory()
+            ->count(5)
+            ->create();
+        $plants = Plants::factory()
+            ->count(5)
+            ->create();
+        $nutdefs = NutrientDeficiencies::factory()
+            ->count(5)
+            ->create();
+        $algaes = Algae::factory()
+            ->count(5)
+            ->create();
+        //v Seed The Favourites table
+        foreach ($users as $user){
+            foreach ($plants as $plant){
+                FavPlant::factory([
+                    'user_id' => $user->id,
+                    'plants_id' => $plant->id
+                ]) ->create();
+            }
+        }
+
+        foreach ($users as $user){
+            foreach ($nutdefs as $nutdef){
+                FavNutDef::factory([
+                    'user_id' => $user->id,
+                    'nutdef_id' => $nutdef->id
+                ]) ->create();
+            }
+        }
+
+        foreach ($users as $user){
+            foreach ($algaes as $algae){
+                FavAlgae::factory([
+                    'user_id' => $user->id,
+                    'algae_id' => $algae->id
+                ]) ->create();
+            }
+        }
 
         // User::factory()
         // ->has(FavPlant::factory(),'fav_Plant')
@@ -49,12 +73,12 @@ class DatabaseSeeder extends Seeder
         // ->count(5)
         // ->create();
         //? ///////////////////////////////////////
-        
+
         //v Default Seed
-        User::factory(5)->create();
-        Plants::factory(5)->create();
-        Algae::factory(5)->create();
-        NutrientDeficiencies::factory(5)->create();
+        // User::factory(5)->create();
+        // Plants::factory(5)->create();
+        // Algae::factory(5)->create();
+        // NutrientDeficiencies::factory(5)->create();
         //v /////////////////////////////////////////
     }
 }
