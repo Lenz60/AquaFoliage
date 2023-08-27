6931fc70f88cef7f76aaf8fc376c034dde6ff9b6
Rafly Andrian <raflyandrian@gmail.com>
2023-08-28 03:51:17 +0700

    named route of docs

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::middleware('auth')->group(function () {
 Route::controller(ContentExcerptController::class)->group(function () {
     Route::get('/excerpt','index')->name('ContentExcerpt');
 });
-Route::get('docs', [PlantsController::class, "index"]);
+Route::get('docs', [PlantsController::class, "index"])->name('plants');
 Route::get('loginOld', [UserController::class, "indexLogin"])->name('login');
 Route::post('login/auth', [UserController::class, "login"])->name('loginAuth');
 Route::get('registerOld', [UserController::class, "register"])->name('register');
