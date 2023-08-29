c30cf5810b09b46527f9e4e6222d1bb3bc05858b
Rafly Andrian <raflyandrian@gmail.com>
2023-08-30 03:47:40 +0700

    change style of content excerpt to 404 because there is nothing there (WIP)

--- resources/js/Pages/Components/Plants/Content/ContentExcerpt.vue
+++ resources/js/Pages/Components/Plants/Content/ContentExcerpt.vue
@@ -1,7 +1,28 @@
 <template>
-    <div>
-        <h3 class="text-black">Content Excerpt</h3>
-        <p>{{ content }}</p>
+    <div class="bg-neutral">
+        <!-- <h3 class="text-black">Content Excerpt</h3>
+        <p>{{ content }}</p> -->
+        <div
+            class="flex flex-col justify-center items-center h-screen w-screen"
+        >
+            <div class="items-center justify-center text-center">
+                <h1
+                    class="text-[200px] font-montserrat text-center text-primary"
+                >
+                    404
+                </h1>
+                <h2 class="text-[40px] font-montserrat text-primary pb-5">
+                    Not Found
+                </h2>
+            </div>
+            <div class="items-center justify-content-center">
+                <h3
+                    class="text-[20px] font-montserrat text-center text-primary"
+                >
+                    The page you requested is not found
+                </h3>
+            </div>
+        </div>
     </div>
 </template>
 
