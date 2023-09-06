c5afcfe087082f4d040b5c03b74591f8596b03d2
Rafly Andrian <raflyandrian@gmail.com>
2023-09-06 20:04:55 +0700

    Passing state props and favdata

--- resources/js/Pages/Components/Plants/Content/DetailPlants.vue
+++ resources/js/Pages/Components/Plants/Content/DetailPlants.vue
@@ -2,15 +2,30 @@
     <div>
         <div v-if="Desc == 'plants'">
             <Head title="Plants Descriptions"></Head>
-            <PlantsDesc :Payload="Payload" :Content="Desc"></PlantsDesc>
+            <PlantsDesc
+                :Payload="Payload"
+                :Content="Desc"
+                :FavData="FavData"
+                :State="State"
+            ></PlantsDesc>
         </div>
         <div v-else-if="Desc == 'nutDef'">
             <Head title="Nutrient Deficiencies Descriptions"></Head>
-            <NutDefDesc :Payload="Payload" :Content="Desc"></NutDefDesc>
+            <NutDefDesc
+                :Payload="Payload"
+                :Content="Desc"
+                :FavData="FavData"
+                :State="State"
+            ></NutDefDesc>
         </div>
         <div v-else>
             <Head title="Algae Descriptions"></Head>
-            <AlgaeDesc :Payload="Payload" :Content="Desc"></AlgaeDesc>
+            <AlgaeDesc
+                :Payload="Payload"
+                :Content="Desc"
+                :FavData="FavData"
+                :State="State"
+            ></AlgaeDesc>
         </div>
     </div>
 </template>
@@ -29,9 +44,10 @@ export default {
         NutDefDesc,
         AlgaeDesc,
     },
-    props: ["Desc", "Payload"],
+    props: ["Desc", "Payload", "State", "FavData"],
     setup(props) {
         // console.log(props.Desc);
+        // console.log(props.FavData["favPlant"]);
         onMounted(() => {
             // console.log(this.name);
         });
